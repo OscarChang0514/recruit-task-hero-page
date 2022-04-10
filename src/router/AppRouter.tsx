2810a0639395f85024b55fc29f6ebab4dfa4200e
Oscar Chang <y520945811@gmail.com>
2022-04-10 22:22:38 +0800

    router add no match redirect

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,27 +1,26 @@
-import { Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "src/components/Layout";
 import { RouteConfig } from "./AppRouter.type";
 import routeConfigs from "./route-config";
 
-const generateRoute = (routes: RouteConfig[]) => routes.map(({path, element, childs}) => {
-    let Element = element;
-    return <Route path={path} element={<Element />}>{childs ? generateRoute(childs) : null}</Route>
+const generateRoute = (routes: RouteConfig[]) => routes.map(({ path, element, childs }) => {
+	let Element = element;
+	return <Route path={path} element={<Element />}>{childs ? generateRoute(childs) : null}</Route>
 });
 
 const AppRouter: React.FC = () => {
 
-    return(
-        <BrowserRouter >
-            <Suspense fallback={<></>}>
-                <Routes>
-                    <Route path={'/'} element={<Layout />}>
-                        {generateRoute(routeConfigs)}
-                    </Route>
-                </Routes>
-            </Suspense>
-        </BrowserRouter >
-    );
+	return (
+		<BrowserRouter >
+			<Routes>
+				<Route path={'/'} element={<Layout />}>
+					{generateRoute(routeConfigs)}
+				</Route>
+				{/* 無對應的url時導頁 */}
+				<Route path={'*'} element={<Navigate to="/" replace />}/>
+			</Routes>
+		</BrowserRouter >
+	);
 }
 
 export default AppRouter;
