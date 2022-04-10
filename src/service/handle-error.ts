8266e3ecc0c47a600c513278ead469ad8a378124
Oscar Chang <y520945811@gmail.com>
2022-04-10 22:11:18 +0800

    base deal with api error

--- src/service/handle-error.ts
+++ src/service/handle-error.ts
@@ -2,6 +2,9 @@ import { Observable, of } from 'rxjs';
 
 export default function handleError<T>(err: any, caught: Observable<T>): Observable<any> {
 
+    alert('網頁出現錯誤，即將導回主頁');
+    window.location.reload();
+    window.location.href = '/';
     console.error(err);
 
    return of();
