591a5780f01eff3cd37afadc27c436ab1742ac43
Oscar Chang <y520945811@gmail.com>
2022-04-11 21:17:07 +0800

    route use lazy loading

--- src/router/route-config.tsx
+++ src/router/route-config.tsx
@@ -1,24 +1,22 @@
-import HeroList from "src/pages/HeroList";
-import HeroProfile from "src/pages/HeroProfile";
-import Home from "src/pages/Home";
+import { lazy } from "react";
 import { RouteConfig } from "./AppRouter.type";
 
 export default [
-	{
-		path: '/',
-		title: 'Home',
-		index: true,
-		element: Home, 
-	},
-	{
-		path: '/heroes', 
-		title: 'Heroes',
-		element: HeroList, 
-		childs: [
-			{ 
-				path: '/heroes/:heroId', 
-				element: HeroProfile
-			}
-		]
-	},
+  {
+    path: '/',
+    title: 'Home',
+    index: true,
+    element: lazy(() => import("src/pages/Home")),
+  },
+  {
+    path: '/heroes',
+    title: 'Heroes',
+    element: lazy(() => import("src/pages/HeroList")),
+    childs: [
+      {
+        path: '/heroes/:heroId',
+        element: lazy(() => import("src/pages/HeroProfile"))
+      }
+    ]
+  },
 ] as RouteConfig[];
