b8448cbe4f4e182de3a874af40567c8ef7954f40
Oscar Chang <y520945811@gmail.com>
2022-04-11 21:44:07 +0800

    Home Rwd

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -1,13 +1,18 @@
 import PrimaryButton from "src/components/PrimaryButton";
+import { $Rwd } from "src/style/rwd";
 import styled from "styled-components";
 
-export const ButtonGroup = styled.div`
+export const MainTitle = styled.h1`
+	font-size: 70px;
 	
-`;
+	@media (max-width: ${$Rwd.tablet}) {  
+		font-size: 50px;
+	}
+`; 
 
 export const StyledPrimaryButton = styled(PrimaryButton)`
 	font-size: 1.2em;
 	padding: 5px 15px;
 	width: 150px;
-	margin: 0 15px;
+	margin: 10px 15px;
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import { ButtonGroup, StyledPrimaryButton } from "./Home.style";
+import { MainTitle, StyledPrimaryButton } from "./Home.style";
 import { HomeProps } from "./Home.type";
 
 const Home: React.FC<HomeProps> = (props) => {
@@ -9,21 +9,17 @@ const Home: React.FC<HomeProps> = (props) => {
   return (
     <div style={{textAlign: 'center'}}>
       <header>
-        <h1 style={{fontSize: '70px'}}>Welcome !</h1>
-        <h4>
-          感謝給予面試機會 !
-        </h4>
-        <h4>
-          請查看Heroes頁面或點選Document跳至Github readme
-        </h4>
-        <ButtonGroup>
+        <MainTitle>Welcome !</MainTitle>
+        <h4>感謝給予面試機會 !</h4>
+        <h4>請查看Heroes頁面或點選Document跳至Github readme</h4>
+        <div>
           <StyledPrimaryButton onClick={() => navigate('/heroes')}>
             Heroes
           </StyledPrimaryButton>
           <StyledPrimaryButton onClick={() => window.open('https://github.com/OscarChang0514/recruit-task-hero-page')}>
             Document
           </StyledPrimaryButton>
-        </ButtonGroup>
+        </div>
       </header>
     </div>
   );
