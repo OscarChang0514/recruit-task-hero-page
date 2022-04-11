1a929ccdcd2f3337afc4bb84c85af712550fa6a5
Oscar Chang <y520945811@gmail.com>
2022-04-11 20:58:03 +0800

    Home page放入正式內容

---
+++ src/pages/Home/Home.style.ts
@@ -0,0 +1,13 @@
+import PrimaryButton from "src/components/PrimaryButton";
+import styled from "styled-components";
+
+export const ButtonGroup = styled.div`
+	
+`;
+
+export const StyledPrimaryButton = styled(PrimaryButton)`
+	font-size: 1.2em;
+	padding: 5px 15px;
+	width: 150px;
+	margin: 0 15px;
+`;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,29 @@
+import { useNavigate } from "react-router";
+import { ButtonGroup, StyledPrimaryButton } from "./Home.style";
 import { HomeProps } from "./Home.type";
 
-
 const Home: React.FC<HomeProps> = (props) => {
+
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome !</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={{textAlign: 'center'}}>
+      <header>
+        <h1 style={{fontSize: '70px'}}>Welcome !</h1>
+        <h4>
+          感謝給予面試機會 !
+        </h4>
+        <h4>
+          請查看Heroes頁面或點選Document跳至Github readme
+        </h4>
+        <ButtonGroup>
+          <StyledPrimaryButton onClick={() => navigate('/heroes')}>
+            Heroes
+          </StyledPrimaryButton>
+          <StyledPrimaryButton onClick={() => window.open('https://github.com/OscarChang0514/recruit-task-hero-page')}>
+            Document
+          </StyledPrimaryButton>
+        </ButtonGroup>
       </header>
     </div>
   );
